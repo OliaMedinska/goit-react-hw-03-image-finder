9430ef0a7b5baf2db795d59861e0913fd4f9e69f
OliaMedinska <medinska2@gmail.com>
2023-11-30 17:58:12 +0100

    api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,29 +2,13 @@ import React, { Component } from "react";
 import { Searchbar } from "../Searchbar/Searchbar";
 import { Loader } from "components/Loader/Loader";
 import { LoaderButton } from "components/Button/Button";
-import { fetchImages } from "api";
 import { ImageGallery } from "components/ImageGallery/ImageGallery";
-// import Notiflix from 'notiflix';
-// import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export class App extends Component {
 state = {
   galleryItems: [],
 };
-
-
-// async componentDidMount() {
-//   try {
-//     const initialImages = await fetchImages();
-//     this.setState({
-//       galleryItems: initialImages,
-//     })
-//   } catch (error) {
-//     console.error("Error fetching images:", error);
-//   }
-// };
-
+// 
 render() {
   const { galleryItems } = this.state;
 
@@ -37,8 +21,4 @@ return (
   </>
 )
 };
-
-
-
-
 };
