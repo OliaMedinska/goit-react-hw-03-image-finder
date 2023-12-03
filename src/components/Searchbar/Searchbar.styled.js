fdca22a1586c22c45899f75f3b03a58213ca7e50
OliaMedinska <medinska2@gmail.com>
2023-12-04 00:32:10 +0100

    icon

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -34,7 +34,7 @@ export const FormButton = styled.button`
     width: 48px;
     height: 48px;
     border: 0;
-    background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+    background-image: url('https://oliamedinska.github.io/goit-js-hw-11/search-icn.f0c8f789.svg');
     background-size: 40%;
     background-repeat: no-repeat;
     background-position: center;
